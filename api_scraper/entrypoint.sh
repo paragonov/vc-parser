979a9047a2d79966d55e6a7eda3f1cfeee00aed7
vladimir <vladimirhoris@gmail.com>
2022-10-14 14:18:16 +0700

    add create superuser

--- api_scraper/entrypoint.sh
+++ api_scraper/entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-#if [ "$DATABASE" = "postgres" ]
-#then
-#    echo "Waiting for postgres..."
-#
-#    while ! nc -z $SQL_HOST $SQL_PORT; do
-#      sleep 0.1
-#    done
-#
-#    echo "PostgreSQL started"
-#fi
+
 
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
+
 
 exec "$@"
